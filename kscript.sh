2d1424fbc06bd9da23ae235e280eaa8673fd23ee
Neil Vallecer <neilvallecer12@gmail.com>
2025-02-21 20:18:24 +0800

    Add kscript.sh for system package installation and updates

---
+++ kscript.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install sddm -y
+sudo apt install kde-full -y
+sudo apt install kitty -y 
+sudo apt install fastfetch -y
+sudo apt install ranger -y
+sudo apt install btop -y
+sudo apt install htop -y
